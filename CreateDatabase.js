51386b6406f3dd54f29470d722d300dd44080a5a
kprabhut <kprabhut@andrew.cmu.edu>
2017-02-25 18:21:12 -0800

    Adding comments inthe Database Schema File

--- CreateDatabase.js
+++ CreateDatabase.js
@@ -2,6 +2,8 @@
 
 // Creating Class user
 class User {
+
+    // Constructor for initializing values for a new user
     constructor(id, username, password, status) {
       this.id = id;
       this.username = username;
@@ -9,6 +11,7 @@ class User {
       this.status   = status;
     }
 
+    // Method to create a new user, takes the "USERS" collection as a parameter
     createUser(collection) {
       this.collection = collection;
       collection.insert({
@@ -19,6 +22,7 @@ class User {
       });
     }
 
+    // Method to display a user, takes the "USERS" collection as a parameter
     displayUser(collection) {
       this.collection = collection;
       collection.find({"username" : this.username}).toArray(function(err, results) {
@@ -26,19 +30,25 @@ class User {
       });;
     }
 
+    // Method to delete a user, takes the "USERS" collection as a parameter
     removeUser(collection) {
       this.collection = collection;
       collection.remove({"username" : this.username});
     }
 }
 
+// Creating Message Class
 class Message {
+
+  // Constructor for initializing values for a new message
   constructor(senderid, receiverid, message) {
     this.senderid = senderid;
     this.receiverid = receiverid;
     this.message = message;
   }
 
+  // Method to insert a new message, takes the "MESSAGE" collection as the parameter
+  // If the receiverid is null, it indicates a message that was shared publicly
   createMessage(collection) {
     this.collection = collection;
     collection.insert({
@@ -48,6 +58,7 @@ class Message {
     });
   }
 
+  // Method to display a message, takes the "MESSAGE" collection as the parameter
   displayMessage(collection) {
     this.collection = collection;
     collection.find({"message" : this.message}).toArray(function(err, results) {
@@ -55,22 +66,26 @@ class Message {
     });;
   }
 }
+
+// Database Connection to ESNDB
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://127.0.0.1:27017/ESNDB', function (err, db) {
   if (err) {
     throw err;
   }
   else {
-var collection = db.collection('USERS');
-let test_user = new User("1", "test_username", "test_password", "offline");
-test_user.removeUser(collection);
-test_user.createUser(collection);
-test_user.displayUser(collection);
+    // Initializing and creating a new user
+    var collection = db.collection('USERS');
+    let test_user = new User("1", "test_username", "test_password", "offline");
+    test_user.removeUser(collection);
+    test_user.createUser(collection);
+    test_user.displayUser(collection);
 
-collection = db.collection('MESSAGES');
-let test_message = new Message("1", "2", "Test message created during testing");
-test_message.createMessage(collection);
-test_message.displayMessage(collection);
-db.close();
-}
+    // Initializing and creating a message
+    collection = db.collection('MESSAGES');
+    let test_message = new Message("1", "2", "Test message created during testing");
+    test_message.createMessage(collection);
+    test_message.displayMessage(collection);
+    db.close();
+  }
 });
