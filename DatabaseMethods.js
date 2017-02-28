c8b3845a080c99d8a91f0189eebdbd123f37a713
Komala Prabhu Tirupachur <kprabhut@andrew.cmu.edu>
2017-02-27 16:55:39 -0800

    Making minor changes

--- DatabaseMethods.js
+++ DatabaseMethods.js
@@ -13,7 +13,7 @@ class User {
     // Method to create a new user, takes the "USERS" collection as a parameter
     createUser(db, callback) {
       this.collection = db.collection('USERS');
-      collection.insert({
+      this.collection.insert({
         "username"  : this.username,
         "password"  : this.password,
         "status"    : this.status
@@ -30,7 +30,7 @@ class User {
     // Method to display a user, takes the "USERS" collection as a parameter
     displayUser(db, callback) {
       this.collection = db.collection('USERS');
-      collection.find({"username" : this.username}).toArray(function(err, results) {
+      this.collection.find({"username" : this.username}).toArray(function(err, results) {
         if(err)
         {
             console.dir('Error in displaying the user'+ err);
@@ -46,7 +46,7 @@ class User {
     // Method to delete a user, takes the "USERS" collection as a parameter
     removeUser(db, callback) {
       this.collection = db.collection('USERS');
-      collection.remove({"username" : this.username}, function(err, results) {
+      this.collection.remove({"username" : this.username}, function(err, results) {
         if(err)
         {
             console.dir('Error creating a new user'+ err);
@@ -62,7 +62,7 @@ class User {
     // Updates the status of a user
     updateStatus(db, username, status, callback) {
       this.collection = db.collection('USERS');
-      collection.update({"username" : username}, {$set : { "status" :  status}}, function(err, results) {
+      this.collection.update({"username" : username}, {$set : { "status" :  status}}, function(err, results) {
         if(err)
         {
             console.dir('Error updating the status ' + err);
@@ -78,7 +78,7 @@ class User {
     // Retrieves the users based on the status passed
     displayStatusUsers(db, status, callback) {
       this.collection = db.collection('USERS');
-      collection.find({"status" : status}, function(err, results) {
+      this.collection.find({"status" : status}, function(err, results) {
         if(err)
         {
             console.dir('Error updating the status ' + err);
@@ -94,7 +94,7 @@ class User {
     // Retrieves all the users
     displayUsers(db, callback) {
       this.collection = db.collection('USERS');
-      collection.find().toArray(function(err, results) {
+      this.collection.find().toArray(function(err, results) {
         if(err)
         {
             console.dir('Error updating the status ' + err);
@@ -110,7 +110,7 @@ class User {
     // Method to check if a user exists
     checkUser(db, username, callback) {
       this.collection = db.collection('USERS');
-      collection.find({"username" : username}).toArray(function(err, result) {
+      this.collection.find({"username" : username}).toArray(function(err, results) {
         if(err)
         {
             console.log('Error in retrieving user'+ err);
@@ -126,7 +126,7 @@ class User {
     // Method to authenticate the user
     checkPassword(db, username, password, callback) {
       this.collection = db.collection('USERS');
-      collection.find({"username" : username, "password" : password}).toArray(function(err, result) {
+      this.collection.find({"username" : username, "password" : password}).toArray(function(err, result) {
         if(err)
         {
             console.log('Error in authenticating user'+ err);
@@ -157,7 +157,7 @@ class Message {
   // The type can have values "Public", "Private" or "GroupName"
   insertMessage(db, callback) {
     this.collection = db.collection('MESSAGES');
-    collection.insert({
+    this.collection.insert({
       "sender"    : this.sender,
       "receiver"  : this.receiver,
       "type"     : this.type,
@@ -180,7 +180,7 @@ class Message {
   // Method to display all the messages
   displayMessages(db, callback) {
     this.collection = db.collection('MESSAGES');
-    collection.find().toArray(function(err, results) {
+    this.collection.find().toArray(function(err, results) {
       if(err)
       {
           console.dir('Error inserting the message ' + err);
