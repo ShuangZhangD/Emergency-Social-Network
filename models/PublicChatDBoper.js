621ccdfa8f79988ed5a605698ecf81deb9d041c4
ShuangZhangD <zhangshuanghust@gmail.com>
2017-03-18 01:44:38 -0700

    Merge branch 'komala-iteration-2' of https://github.com/cmusv-fse/s17-ESN-SV7 into shuang-iteration-1
    # Conflicts:
    #	EmergencySocialNetwork/controller/JoinCommunityCtrl.js
    #	EmergencySocialNetwork/public/javascripts/joinCommunity.js

--- models/PublicChatDBoper.js
+++ models/PublicChatDBoper.js
@@ -4,7 +4,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var express = require('express');
-var url = 'mongodb://localhost:27017/test2';
+var url = 'mongodb://root:1234@ds135700.mlab.com:35700/esnsv7';
 
 var db_err_msg = "Database Error";
 
