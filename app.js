4d099393e9017f5e233693428902dcebfc41f9ec
keqinli <likeqing0301@gmail.com>
2017-03-19 19:31:32 -0700

    update controller and dboper of sharestatus part
    # Conflicts:
    #	bower.json
    #	controller/JoinCommunityCtrl.js
    #	public/javascripts/joinCommunity.js
    #	public/javascripts/postAnnoucementCtrl.js
    #	public/stylesheets/ShareStatus.css
    #	views/DisplayDirectory.html
    #	views/ShareStatus.html
    #	views/postAnnouncement.ejs

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/public', PublicChatCtrl.LoadPublicMessage);
 app.post('/public', PublicChatCtrl.AddPublicMessage);
 
 app.get('/privatechat/:sender/:receiver', PrivateChatCtrl.LoadPrivateHistoryMessage);
-app.post('/privatechat', PrivateChatCtrl.AddPrivateMessage);x
+app.post('/privatechat', PrivateChatCtrl.AddPrivateMessage);
 
 
 // catch 404 and forward to error handler
