de9288a49cb52cc76733627e136a28beeee12446
ShuangZhangD <zhangshuanghust@gmail.com>
2017-03-20 17:14:56 -0700

    Post Announcement backend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var io = require('socket.io').listen(server);
 server.listen(process.env.PORT || 5000);
 var JoinCommunityCtrl = require('./controller/JoinCommunityCtrl.js');
 var PublicChatCtrl = require('./controller/PublicChatCtrl.js');
-var PrivateChatCtrl = require('./controller/PrivateChatCtrl.js');
+// var PrivateChatCtrl = require('./controller/PrivateChatCtrl.js');
 var PostAnnouncementCtrl = require('./controller/PostAnnouncementCtrl.js');
 var ShareStatusCtrl = require('./controller/ShareStatusCtrl');
 
@@ -56,9 +56,9 @@ app.post('/post_announcement', PostAnnouncementCtrl.AddAnnouncement);
 app.post('/userstatus', ShareStatusCtrl.AddShareStatus);
 app.get('/userstatus', ShareStatusCtrl.GetShareStatus);
 
-app.get('/privatechat/:sender/:receiver', PrivateChatCtrl.LoadPrivateHistoryMessage);
-app.post('/privatechat', PrivateChatCtrl.AddPrivateMessage);
-app.get('/privatechat/:receiver', PrivateChatCtrl.getCount_IndividualPrivateSender);
+// app.get('/privatechat/:sender/:receiver', PrivateChatCtrl.LoadPrivateHistoryMessage);
+// app.post('/privatechat', PrivateChatCtrl.AddPrivateMessage);
+// app.get('/privatechat/:receiver', PrivateChatCtrl.getCount_IndividualPrivateSender);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -91,22 +91,22 @@ io.on('connection', function(socket) {
     socket.on('Update Share Status', ShareStatusCtrl.UpdateShareStatusSocket(socket)); //for directory updating status
 
     //when a private message is sent
-    socket.on('Private Message', privateChat.privateMessageSocket(socket, ConnectedSockets));
+    // socket.on('Private Message', privateChat.privateMessageSocket(socket, ConnectedSockets));
 
     //when total number of unread(private+public) message is needed
-    socket.on('GetCount AllUnreadMsg', privateChat.getCount_AllUnreadMsg(socket));
+    // socket.on('GetCount AllUnreadMsg', privateChat.getCount_AllUnreadMsg(socket));
 
     //when total number of private unread message is needed
-    socket.on('GetCount AllPrivateUnreadMsg', privateChat.getCount_AllPrivateUnreadMsg(socket));
+    // socket.on('GetCount AllPrivateUnreadMsg', privateChat.getCount_AllPrivateUnreadMsg(socket));
 
     //when individual number of unread message is needed
-    socket.on('GetCount IndividualUnreadMsg', privateChat.getCount_IndividualPrivateUnreadMsg(socket));
+    // socket.on('GetCount IndividualUnreadMsg', privateChat.getCount_IndividualPrivateUnreadMsg(socket));
 
     //when individual latest msg of unread message is needed
-    socket.on('GetMsg IndividualLatestUnreadMsg', privateChat.get_IndividualPrivateUnreadMsg(socket));
+    // socket.on('GetMsg IndividualLatestUnreadMsg', privateChat.get_IndividualPrivateUnreadMsg(socket));
 
     //set the private msg of sender and receiver to be read
-    socket.on('PrivateMsgRead', privateChat.MarkedAsRead());
+    // socket.on('PrivateMsgRead', privateChat.MarkedAsRead());
 
     socket.on('userJoinCommunity', function(username){
       socket.broadcast.emit("userJoined",username);
