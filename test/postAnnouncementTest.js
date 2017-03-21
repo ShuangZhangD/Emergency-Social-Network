e3f470f83e5cbea252c6a16f927f2f919d4b34af
keqinli <likeqing0301@gmail.com>
2017-03-20 17:31:03 -0700

    test part # Conflicts: #	npm-debug.log

--- test/postAnnouncementTest.js
+++ test/postAnnouncementTest.js
@@ -4,11 +4,19 @@
 
 var expect = require('expect.js');
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var express = require('express');
+
+var url = 'mongodb://root:1234@ds135700.mlab.com:35700/esnsv7';
+
 suite('Post Announcement Tests', function(){
+
+    suiteSetup()
 
     test('Creating An Announcement in DB', function(done){
         console.log('Test Creating An Announcement in DB');
-
+        expect(2).to.eql(2);
         done();
     });
 })
