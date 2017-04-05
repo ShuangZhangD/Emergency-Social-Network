c1d676f996c1bc645b2057b315e27c7b4a379f7e
kprabhut <kprabhut@andrew.cmu.edu>
2017-04-04 22:50:42 -0700

    Unit and integration tests separated

--- joinCommunityUnitTests.js
+++ joinCommunityUnitTests.js
@@ -18,7 +18,7 @@ var app = express();
 
 
 //var url = 'mongodb://root:1234@ds137730.mlab.com:37730/esnsv7';//url = 'mongodb://root:1234@ds135690.mlab.com:35690/esntest';
-var DBConfig = require("../controller/DBConfig");
+var DBConfig = require("./TestDBConfig");
 let dbconfig = new DBConfig();
 var url = dbconfig.getURL();
 var error_url = "mongodb://root:123@ds137730.mlab.com:37730/esns";
@@ -29,86 +29,6 @@ var server = request.agent("https://quiet-peak-31270.herokuapp.com");
 
 suite('Join Comunity Integration Tests', function(){
     this.timeout(15000);
-
-    test('Login by RESTful Api', function(done) {
-        server.post('/signup')
-            .send({"username": "test_user_for_rest_api", password: "1234"})
-            .expect(200, function(err, res) {
-                server.post('/login')
-                    .send({"username": "test_user_for_rest_api", password: "1234"})
-                    .expect(200, function(err, res){
-                        if(err) {
-                            return done(err);
-                        }
-                        else {
-                            var idx = res.body.data.indexOf("test_user_for_rest_api");
-                            expect(idx).to.above(-1);
-                            done();
-                        }
-                    });
-            });
-    });
-
-    test('Signup by RESTful Api', function(done){
-        dboper.RemoveUser("test_user_for_rest_api", url, function(success_statuscode, results) {
-            server.post('/signup')
-                .send({"username": "test_user_for_rest_api", password: "1234"})
-                .expect(200, function(err, res){
-                    if(err) {
-                        return done(err);
-                    }
-                    else {
-                        var idx = res.body.data.indexOf("test_user_for_rest_api");
-                        expect(idx).to.above(-1);
-                        done();
-                    }
-                });
-        });
-    });
-
-    test('Userlist by RESTful Api', function(done) {
-        server.post('/signup')
-            .send({"username": "test_user_for_rest_api", password: "1234"})
-            .expect(200, function(err, res) {
-                server.get('/userlist')
-                    .expect(200, function(err, res){
-                        if(err) {
-                            return done(err);
-                        }
-                        else {
-                            var idx = res.body.data1.indexOf("test_user_for_rest_api");
-                            if (idx < 0) {
-                                idx = res.body.data2.indexOf("test_user_for_rest_api");
-                            }
-                            expect(idx).to.above(-1);
-                            done();
-                        }
-                    });
-            });
-    });
-
-    test('Logout by RESTful Api', function(done) {
-        server.post('/signup')
-            .send({"username": "test_user_for_rest_api", password: "1234"})
-            .expect(200, function(err, res) {
-                server.post('/logout')
-                    .send({"username": "test_user_for_rest_api"})
-                    .expect(200, function(err, res){
-                        server.get('/userlist')
-                            .expect(200, function(err, res){
-                                if(err) {
-                                    return done(err);
-                                }
-                                else {
-                                    var idx = res.body.data2.indexOf("test_user_for_rest_api");
-                                    expect(idx).to.above(-1);
-                                    done();
-                                }
-                            });
-
-                    });
-            });
-    });
 
     test("Remove User from Community", function(done){
         dboper.RemoveUser("testuser", url, function (statuscode1, content1){
