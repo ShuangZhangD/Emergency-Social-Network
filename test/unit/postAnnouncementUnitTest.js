673f786a164ea3429ce4b57079d0657080fefd48
huiliangling <helen14.su@hotmail.com>
2017-04-29 21:48:15 -0700

    fix test function not consistent

--- test/unit/postAnnouncementUnitTest.js
+++ test/unit/postAnnouncementUnitTest.js
@@ -54,7 +54,7 @@ suite('Post Announcement Unit Tests', function(){
     });
 
     test('Testing Announcement Function', function(done){
-        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), url, function (err, results1){
+        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), "Active", url, function (err, results1){
             dboper.LoadAnnouncement(url, function (err, results2) {
                expect(results2[results2.length-1]["announcement"]).to.equal("testing announcement function in Unit Test");
                done();
@@ -63,7 +63,7 @@ suite('Post Announcement Unit Tests', function(){
     });
 
     test('Testing Announcement Function DB Error', function(done){
-        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), error_url, function(err,result){
+        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), "Active",error_url, function(err,result){
             //dboper.LoadAnnouncement(url, function (err, results2) {
                 expect(err).to.equal(400);
                 done();
@@ -89,7 +89,7 @@ suite('Post Announcement Unit Tests', function(){
     });
 
     test('Test Public Chat Search Function in models', function(done){
-        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), url, function(err0,content0){
+        dboper.InsertAnnouncement("keqin", "testing announcement function in Unit Test", Date.now(), "Active",url, function(err0,content0){
             expect(err0).to.equal(null);
             dboper.SearchPublicAnn(["chat", "hi"], url,function(err1, results1){
                 expect(err1).to.equal(200); //TODO test if results content equal or not
